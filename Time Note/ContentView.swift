96c9a56b3af7397b1f1fe1fbcb58b5cccb680fb4
sadhu-sanjay <sanjaygoswami60@gmail.com>
2023-12-22 11:10:05 +0530

    Initial Commit

---
+++ Time Note/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time Note
+//
+//  Created by Sanjay Goswami on 22/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Note/Time_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_NoteApp.swift
+//  Time Note
+//
+//  Created by Sanjay Goswami on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
